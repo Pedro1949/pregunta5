516bffeabb249faeaf301937a717553d396d5e74
Pedro1949 <142362192+Pedro1949@users.noreply.github.com>
2023-09-16 23:01:36 -0500

    Add files via upload

---
+++ 05.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int tarifaPorHora = 12;
+        int horasTrabajadas = 56;
+        int salarioSemanal = tarifaPorHora * horasTrabajadas;
+        System.out.println("Salario semanal: " + salarioSemanal );
+    }
+}
